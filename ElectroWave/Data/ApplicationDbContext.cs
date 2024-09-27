ca25ac48dbb52c2a6f4645fd40f4b1a67be3a171
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-27 09:44:20 +0300

    Add project files.

---
+++ ElectroWave/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ElectroWave.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectroWave.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ElectroWave/Models/Cateory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectroWave.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
