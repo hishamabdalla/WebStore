0b4de4d911565a76a832be90d75606018675accf
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-27 11:56:21 +0300

    Add Create and Update For Category

--- ElectroWave/Controllers/CategoryController.cs
+++ ElectroWave/Controllers/CategoryController.cs
@@ -16,5 +16,44 @@ namespace ElectroWave.Controllers
             return View(CategoryList);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null||id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = context.Categories.Find(id);
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Update(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
--- ElectroWave/Models/Cateory.cs
+++ ElectroWave/Models/Cateory.cs
@@ -3,9 +3,14 @@
     public class Category
     {
         public int Id { get; set; }
+
         [Required]
+        [Display(Name = "Category Name")]
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [Display(Name ="Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
 
     }
