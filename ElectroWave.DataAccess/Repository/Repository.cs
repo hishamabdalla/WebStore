afc0f66324724d4818adbc20c08ca3808f2281da
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-29 11:31:11 +0300

    Add Repository And IRepository

---
+++ ElectroWave.DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using ElectroWave.DataAccess.Data;
+using ElectroWave.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroWave.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet=_context.Set<T>();   
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query=query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+    }
+}
