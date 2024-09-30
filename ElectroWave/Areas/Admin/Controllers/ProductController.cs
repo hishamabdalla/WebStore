0e4fc94870757d9f5ff54b9a136dd3b9e6640b1d
hishamabdalla <hishvmabdalla@gmail.com>
2024-09-30 12:24:14 +0300

    CRUD Operation For Product

---
+++ ElectroWave/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using ElectroWave.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectroWave.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> ProductsList = _unitOfWork.Product.GetAll().ToList();
+            return View(ProductsList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            return View(productFromDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Updata(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(productFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
