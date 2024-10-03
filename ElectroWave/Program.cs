e51590e7387aea83cfa72f1fe281bea10a3b3532
hishamabdalla <hishvmabdalla@gmail.com>
2024-10-04 00:10:25 +0300

    Load DatTables

--- ElectroWave/Program.cs
+++ ElectroWave/Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option => 
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ ElectroWave/wwwroot/js/Product.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'price', "width": "5%" },
+            { data: 'description', "width": "25%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
