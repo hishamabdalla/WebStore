4fb1597d42a4f8269a9b8ba048d7e8a1724bde0a
hishamabdalla <hishvmabdalla@gmail.com>
2024-10-05 01:18:51 +0300

    Extend Identity User

--- ElectroWave.DataAccess/Data/ApplicationDbContext.cs
+++ ElectroWave.DataAccess/Data/ApplicationDbContext.cs
@@ -35,5 +35,6 @@ namespace ElectroWave.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
--- ElectroWave.DataAccess/Migrations/20241004221727_ExtendIdentityUser.Designer.cs
+++ ElectroWave.DataAccess/Migrations/20241004221727_ExtendIdentityUser.Designer.cs
@@ -4,6 +4,7 @@ using ElectroWave.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElectroWave.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241004221727_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
